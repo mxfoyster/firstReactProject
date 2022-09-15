599b0f8fc86285616ab2708c41aea64944fdb173
Mark Foyster <mxfoyster@yahoo.co.uk>
2022-09-15 10:52:01 +0100

    Improvements to pop up component, still a warning to sort out though!

--- src/PopUp.js
+++ src/PopUp.js
@@ -1,28 +1,48 @@
-import {useRef, useEffect} from 'react';
+import { useEffect} from 'react';
 import './PopUp.css';
 
 function PopUp(props){
+    //we must have a unique id for each instance, we can base it on the Button ID
+    let containerID = props.buttonID + "_container";
+    let closeBtnID = props.buttonID + "_closeBtn";
+
     useEffect(() => {
         //CODE HERE SHOULD ONLY RUN AFTER FIRST MOUNT
         const popUpButton = document.getElementById(props.buttonID)
-        const popUpWindow = document.getElementById("popUpContainer");
-        const closeBtn = document.getElementById("closeBtn");
-        popUpWindow.classList.add("popUpFadeOut");//hide as default
+        const popUpWindow = document.getElementById(containerID);
+        const closeBtn = document.getElementById(closeBtnID);
+        popUpWindow.classList.add("popUpFirstLoad");//hide minus animation to start off
         popUpButton.addEventListener('click', ()=>{openPopUp(popUpWindow)});
         closeBtn.addEventListener('click', ()=>{closePopUp(popUpWindow)});
+        sizePopUp(popUpWindow, props.size);
         // return () => //clean up code can go here
       }, [])  // <-- Empty array triggers this ONCE ONLY behaviour
     
     return(
-        <div id="popUpContainer">
-             <img id="closeBtn" src="./images/closebtn.png" alt="Close Pop Up Window"/>
-            <p>This is a test</p>
+        <div id={containerID} className='popUpContainer'>
+            <img id={closeBtnID} className='closeBtn' src="./images/closebtn.png" alt="Close Pop Up Window"/>
+            {props.children}
         </div>
     )
 
 }
 
+function sizePopUp(puWindow, size){
+    switch(size){
+        case "small":
+            puWindow.classList.add("popUpIsSmall");
+            break;
+        case "med":
+        case "medium":
+            puWindow.classList.add("popUpIsMedium");
+            break;
+        default:
+            puWindow.classList.add("popUpIsLarge");
+    }
+}
+
 function openPopUp(puWindow){
+    puWindow.classList.remove("popUpFirstLoad");
     puWindow.classList.remove("popUpFadeOut");
     puWindow.classList.add("popUpFadeIn");
 }
--- src/content/Page4.js
+++ src/content/Page4.js
@@ -5,10 +5,25 @@ function Page4(){
     return(
         <div id="middle" className="middle">
             <h2>Pop Up Container</h2>
-            <p>This will be a React version of a Pop Up window similar to the one in my component library. Having it as a React component should make it easy to implement in multiple instances, even nested. I should be able to use props for configuration options to make it flexible.</p>
-
-            <button id="testPopUp">Pop Up</button>
-            <PopUp buttonID="testPopUp"/>
+            <p>This is a simple <i>"Proof of concept"</i> React version of a Pop Up window similar to the one in my component library. I wanted one that would be simple to implement in multiple instances. The size is configured using props and the contents of the popup can be placed directly within the component tags as jsx. You can test out what I have so far by pressing the buttons below:</p>
+            <div className="buttonsContainer">
+                <button id="testPopUp">Small Pop Up</button>
+                <button id="testPopUp2">Medium Pop Up</button>
+                <button id="testPopUp3">Large Pop Up</button>
+            </div>
+            
+            <PopUp buttonID="testPopUp" size="small" key="1">
+                <h2>This is a Small Pop Up test</h2>
+                <p>This is some test content just added between the component tags and retrieved by the component via <b><i>props.children</i></b>.</p>
+            </PopUp>
+            <PopUp buttonID="testPopUp2" size="med" key="2">
+                <h2>This is a Medium Pop Up test</h2>
+                <p>This is some test content just added between the component tags and retrieved by the component via <b><i>props.children</i></b>.</p>
+            </PopUp>
+            <PopUp buttonID="testPopUp3" size="large" key="3">
+                <h2>This is a Large Pop Up test</h2>
+                <p>This is some test content just added between the component tags and retrieved by the component via <b><i>props.children</i></b>.</p>
+            </PopUp>
         </div>
     )
 
